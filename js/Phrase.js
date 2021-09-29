c94ea3d6dff30f599a83f05ae8e069223b290352
Zachary Stocks <stocks.zachary@gmail.com>
2021-09-29 09:52:32 -0600

    checkLetter method
    developed the checkLetter method in the Phrase class to check whether user input matches a letter in the phrase or not.

--- js/Phrase.js
+++ js/Phrase.js
@@ -2,40 +2,58 @@
  * Project 4 - OOP Game App
  * Phrase.js */
 
- class Phrase {
-  constructor(phrase) {
-   this.phrase = phrase.toLowerCase();
+class Phrase {
+ constructor(phrase) {
+  this.phrase = phrase.toLowerCase();
+ }
+
+ /**
+  * Creates and appends to the DOM li objects for each character in the phrase
+  */
+ addPhraseToDisplay() {
+  // convert the phrase to an array of characters containing only letters and spaces
+  const characters = this.phrase
+   .split('')
+   .filter(character => /^[a-z ]$/.test(character));
+
+  // create and append li element with the appropriate classes
+  characters.forEach(character => {
+   const li = document.createElement('li');
+
+   if (character === ' ') {
+    li.className = 'space';
+   } else {
+    li.classList.add('hide', 'letter', character);
+   }
+
+   li.textContent = character;
+   document.querySelector('#phrase ul').append(li);
+  });
+ }
+
+ /**
+  * Checks if the input from the user matches a character in the phrase
+  * @param (object) e - the event object
+  * @return (boolean) - true if the input matches one or more characters in the phrase
+  */
+ checkLetter(e) {
+  // handle input from click events
+  if (e.type === 'click') {
+   const character = e.target.textContent;
+   return this.phrase.includes(character) ? true : false;
   }
 
-  /**
-   * Creates and appends to the DOM li objects for each character in the phrase
-   */
-  addPhraseToDisplay() {
-   // convert the phrase to an array of characters containing only letters and spaces
-   const characters = this.phrase
-    .split('')
-    .filter(character => /^[a-z ]$/.test(character));
+  // handle input from keyboard events
+  if (e.type === 'keyup') {
+   const character = e.key.toLowerCase();
 
-   // create and append li element with the appropriate classes
-   characters.forEach(character => {
-    const li = document.createElement('li');
-
-    if (character === ' ') {
-     li.className = 'space';
-    } else {
-     li.classList.add('hide', 'letter', character);
-    }
-
-    li.textContent = character;
-    document.querySelector('#phrase ul').append(li);
-   });
-  }
-
-  checkLetter() {
-
-  }
-
-  showMatchedLetter() {
-
+   if (/^[a-z]$/.test(character)) {
+    return this.phrase.includes(character) ? true : false;
+   }
   }
  }
+
+ showMatchedLetter() {
+
+ }
+}
--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
- const myPhrase = new Phrase('Jacqalyn is amazing!');
- myPhrase.addPhraseToDisplay();
+const keyboard = document.querySelector('#qwerty');
+
+const myPhrase = new Phrase('Jacqalyn is amazing!');
+myPhrase.addPhraseToDisplay();
+
+keyboard.addEventListener('click', (e) => {
+ myPhrase.checkLetter(e);
+});
+
+document.addEventListener('keyup', (e) => {
+ myPhrase.checkLetter(e);
+});
