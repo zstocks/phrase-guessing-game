5eaaf08eb405a6927e792c81f082384529d32cf3
Zachary Stocks <stocks.zachary@gmail.com>
2021-09-29 12:40:54 -0600

    Game class constructor
    Set up the constructor for the Game class. Laid out methods to be developed in the Game class.

--- js/Game.js
+++ js/Game.js
@@ -3,5 +3,39 @@
  * Game.js */
 
  class Game {
-  
+  constructor() {
+   this.missed = 0;
+   this.phrases = [
+    'Frogs are amphibians',
+    'Deer are mammals',
+    'Snakes are reptiles',
+    'Kangaroos are marsupials',
+    'Zoology is fun'
+   ];
+   this.activePhrase = null;
+  }
+
+  startGame() {
+   document.querySelector().style.display = 'none';
+  }
+
+  getRandomPhrase() {
+
+  }
+
+  handleInteraction() {
+
+  }
+
+  removeLife() {
+
+  }
+
+  checkForWin() {
+
+  }
+
+  gameOver() {
+
+  }
  }
